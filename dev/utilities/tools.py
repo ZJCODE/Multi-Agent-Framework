5252c1d40fc763bc792ebadf09c47f6952bc4bd9
ZhangJun <zhangjun310@live.com>
2024-12-12 18:32:37 +0800

    update

--- dev/utilities/tools.py
+++ dev/utilities/tools.py
@@ -1,5 +1,9 @@
 from duckduckgo_search import DDGS
+import requests
+from bs4 import BeautifulSoup
 # pip install duckduckgo_search
+# pip install requests
+# pip install beautifulsoup4
 
 def web_search(query:str,timelimit:str,max_results:int):
     """
@@ -13,3 +17,31 @@ def web_search(query:str,timelimit:str,max_results:int):
     """
     results = DDGS().text(keywords=query, safesearch='off', timelimit=timelimit, max_results=max_results)
     return results
+
+def web_content_process(link: str) -> str:
+    """
+    Process the content of the given link by using beautifulsoup.
+
+    Args:
+        link (str): The link to process.
+
+    Returns:
+        str: The main content of the link.
+    """
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Attempt to find the main content using common tags
+    main_content = ''
+    if soup.main:
+        main_content = soup.main.get_text(strip=True)
+    elif soup.article:
+        main_content = soup.article.get_text(strip=True)
+    elif soup.section:
+        main_content = soup.section.get_text(strip=True)
+    elif soup.body:
+        main_content = soup.body.get_text(strip=True)
+    else:
+        main_content = soup.get_text(strip=True)
+
+    return main_content
